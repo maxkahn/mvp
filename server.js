c553287e55533d897c0ec9b509f19a563834d9d5
Max Kahn <kahn.max@gmail.com>
2016-01-04 12:31:44 -0800

    mongodb database connected to server; server runs. Worried about deployment, but at least local solution.

--- server.js
+++ server.js
@@ -2,7 +2,24 @@ var express = require('express');
 var session = require('express-session');
 var bcrypt = require('bcrypt-nodejs');
 //what do I need to set up my db?
-var db = require('mongodb');
+var MongoClient = require('mongodb');
+var mongoose = require('mongoose');
+var Admin = mongoose.mongo.Admin;
+
+//this is code we'll have to change when we deploy
+//not sure I need to do this
+//if db does not exist; we'll start creating collections, right?
+//and that will make it exist
+//do I get any security features at all?
+//also, not totally clear on createConnection vs connect
+var connection = mongoose.createConnection('mongodb://localhost:27017/app');
+
+// connection.on('open', function() {
+//   new Admin(connection.db).listDatabases(function (err, result) {
+//     console.log('listDatabases succeeded');
+//     var allDatabases = result.databases;
+//   });
+// });
 
 var app = express();
 
@@ -10,8 +27,7 @@ app.get('/', function(req, res) {
   res.sendfile('index.html');
 })
 
-//remember that I'll need to change this
-//when I deploy
+//remember that I'll need to change this when I deploy
 var port = 3000;
 
 app.listen(port, function() {
